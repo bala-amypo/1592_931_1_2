4cece75514a90a23a479c51986b709fd2e1a949c
bala-amypo <bala@amypo.in>
2025-12-15 16:28:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/NotificationLog.java
+++ src/main/java/com/example/demo/entity/NotificationLog.java
@@ -1,7 +1,62 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 public class NotificationLog {
     private long id;
     private Broadcast broadcast;
-    private 
+    private String recipientEmail;
+    private LocalDateTime sentOn;
+    private String status;
+
+    public NotificationLog(){}
+
+    public NotificationLog(Broadcast broadcast, String recipientEmail, LocalDateTime sentOn, String status) {
+        this.broadcast = broadcast;
+        this.recipientEmail = recipientEmail;
+        this.sentOn = sentOn;
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Broadcast getBroadcast() {
+        return broadcast;
+    }
+
+    public void setBroadcast(Broadcast broadcast) {
+        this.broadcast = broadcast;
+    }
+
+    public String getRecipientEmail() {
+        return recipientEmail;
+    }
+
+    public void setRecipientEmail(String recipientEmail) {
+        this.recipientEmail = recipientEmail;
+    }
+
+    public LocalDateTime getSentOn() {
+        return sentOn;
+    }
+
+    public void setSentOn(LocalDateTime sentOn) {
+        this.sentOn = sentOn;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
 }
