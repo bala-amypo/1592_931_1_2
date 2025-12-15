4aa16dbeb88138985d969e5a0eee4933793e7094
bala-amypo <bala@amypo.in>
2025-12-15 17:37:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.UserService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -10,4 +13,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Auth Controller")
 public class AuthController {
     
+    @Autowired
+    UserService userService;
 }
--- src/main/java/com/example/demo/controller/BroadcastController.java
+++ src/main/java/com/example/demo/controller/BroadcastController.java
@@ -1,8 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.example.demo.service.BroadcastService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @ResponseStatus
+@RequestMapping("/broadcasts")
+@Tag(name = "Broadcast Controller")
+
 public class BroadcastController {
-    
+    @Autowired
+    BroadcastService broadcastService;
 }
