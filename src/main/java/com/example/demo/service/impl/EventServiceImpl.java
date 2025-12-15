3673ca59b4dda6d499e93a6265a0d7362b4d9cbb
bala-amypo <bala@amypo.in>
2025-12-15 17:52:08 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EventServiceImpl.java
@@ -1,10 +1,29 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Event;
 import com.example.demo.service.EventService;
 
 @Service
 public class EventServiceImpl implements EventService  {
-    
+    Event e;
+    List<Event> list;
+    public Event createEvent(Event event,long organizedId){
+        return e;
+    }
+    public List<Event> getAllPublicEvents(){
+        return list;
+    }
+    public List<Event> getEventsByOrganizer(long organizedId){
+        return list;
+    }
+    public Event geEvent(long eventId){
+        return e;
+    }
+    public String deleteEvent(long eventId,long organizerId){
+        return "Hi";
+    }
 }
--- src/main/java/com/example/demo/service/impl/SubscriberServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SubscriberServiceImpl.java
@@ -1,10 +1,22 @@
 package com.example.demo.service.impl;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.User;
+import com.example.demo.entity.Subscriber;
 import com.example.demo.service.SubscriberService;
 
 @Service
 public class SubscriberServiceImpl implements SubscriberService {
-    
+    Subscriber s;
+    List<Subscriber> list;
+    public Subscriber registSubscriber(Subscriber sub){
+        return s;
+    }
+    public List<Subscriber> getAllSubscribers(){
+        return list;
+    }
+    public List<Subscriber> findMatchingSubscribers(String category,String location){
+        return list;
+    }   
 }
