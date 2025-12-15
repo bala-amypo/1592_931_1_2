228fbfb9995c0fda6dc9231580bfa8386171b4fb
bala-amypo <bala@amypo.in>
2025-12-15 16:21:07 +0000

    User commit

--- src/main/java/com/example/demo/entity/Event.java
+++ src/main/java/com/example/demo/entity/Event.java
@@ -1,5 +1,113 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 public class Event {
+    private long id;
+    private String title;
+    private String description;
+    private String category;
+    private String location;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private User organizer;
+    private LocalDateTime createdOn;
+    private boolean isPublic;
+
+    public Event(){}
+
+    public Event(String title, String description, String category, String location, LocalDateTime startTime,
+            LocalDateTime endTime, User organizer, LocalDateTime createdOn, boolean isPublic) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.location = location;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.organizer = organizer;
+        this.createdOn = createdOn;
+        this.isPublic = isPublic;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public User getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(User organizer) {
+        this.organizer = organizer;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean isPublic) {
+        this.isPublic = isPublic;
+    }
     
+
 }
