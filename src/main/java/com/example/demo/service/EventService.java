2a5eda5f84ed077cee00b737513f48734b55efdf
bala-amypo <bala@amypo.in>
2025-12-15 16:42:08 +0000

    User commit

--- src/main/java/com/example/demo/service/EventService.java
+++ src/main/java/com/example/demo/service/EventService.java
@@ -1,7 +1,15 @@
 package com.example.demo.service;
 
+
+
+import java.util.List;
+
 import com.example.demo.entity.Event;
 
 public interface EventService {
-    Event createEvent(Event event,long organizedId);   
+    Event createEvent(Event event,long organizedId); 
+    List<Event> getAllPublicEvents();
+    List<Event> getEventsByOrganizer(long organizedId);
+    Event geEvent(long eventId);
+    String deleteEvent(long eventId,)
 }
