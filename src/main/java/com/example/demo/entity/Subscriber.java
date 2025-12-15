5dff094b7b66bd4d21e7784bc1faffffd94191e8
bala-amypo <bala@amypo.in>
2025-12-15 16:22:07 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Subscriber.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+public class Subscriber {
+    
+    private long id;
+    private String name;
+    private 
+    private
+    private
+    private
+    private
+}
