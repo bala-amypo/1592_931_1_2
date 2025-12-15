d1eec0b4c4b3c756229fe5c3080a9c3473053f8d
bala-amypo <bala@amypo.in>
2025-12-15 16:47:08 +0000

    User commit

--- src/main/java/com/example/demo/service/BroadcastService.java
+++ src/main/java/com/example/demo/service/BroadcastService.java
@@ -1,5 +1,12 @@
 package com.example.demo.service;
 
-public class BroadcastService {
-    
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.example.demo.entity.Broadcast;
+
+public interface BroadcastService {
+    Broadcast scheduleBroadcast(long eventId,String channel,LocalDateTime scheduledOn);
+    Broadcast runBroadcast(long broadcastId);
+    List<Broadcast> getBroadcastsByEvent(long eventId);
 }
